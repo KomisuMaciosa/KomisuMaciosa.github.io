bd1156ffa16ffe7ea919d927e5fd5f95b0d0a1ec
KomisuMaciosa <108614829+KomisuMaciosa@users.noreply.github.com>
2022-07-03 14:10:03 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,4 @@
+function showMessage()
+{
+    document.getElementById("message").innerHTML = "Świetnie!!! Transakcja przebiegła pomyślnie. Wybrany pojazd wkrótce trafi na stronę";
+}
---
+++ skrypt2.js
@@ -0,0 +1,4 @@
+function showMessage()
+{
+    document.getElementById("message").innerHTML = "Świetnie!!! Transakcja przebiegła pomyślnie. Wybrany pojazd wkrótce zniknie ze strony";
+}
